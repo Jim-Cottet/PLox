43634febd83989febd54f15a2cd8d15dc41d3308
Jim-Cottet <jimcottet.pro@gmail.com>
2024-10-17 07:56:35 +0200

    Init depo

---
+++ main.py
@@ -0,0 +1,11 @@
+from Parser import Parser
+
+def main():
+    # Create an instance of the Parser class
+    parser = Parser()
+    
+    # Call a method from the Parser class (assuming a method named 'parse' exists)
+    parser.parse()
+
+if __name__ == "__main__":
+    main()
